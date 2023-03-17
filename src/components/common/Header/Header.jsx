9426417592bd19f27483a0fb9c505662904d84bf
Ramiro <67852209+MRAMINIKO@users.noreply.github.com>
2023-03-17 00:02:17 -0300

    refactor: no se como hacer el header
    me voy a dormir, no se de que estilo hacer el header

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,30 +1,9 @@
 import React from "react";
-import { ListGroup } from "react-bootstrap";
-import { Link } from "wouter";
 
 export function Header() {
   return (
-    <header className="p-2 d-flex justify-content-between border border-bottom">
-      <div className="">
-        <Link to="/prueba">Prueba</Link>
-      </div>
-      <ListGroup className="list-group-horizontal">
-        <ListGroup.Item>
-          <Link to="/" className="text-decoration-none text-dark">
-            Inicio
-          </Link>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Link to="/about" className="text-decoration-none text-dark">
-            Sobre la seju
-          </Link>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Link to="/login" className="text-decoration-none text-dark">
-            Iniciar sesión
-          </Link>
-        </ListGroup.Item>
-      </ListGroup>
+    <header className="p-2 border border-bottom">
+      <h1>a</h1>
     </header>
   );
 }
