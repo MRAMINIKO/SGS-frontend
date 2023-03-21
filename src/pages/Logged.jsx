2a0168a316df9e9e5d91754f38048403650b1f5e
Ramiro <67852209+MRAMINIKO@users.noreply.github.com>
2023-03-20 23:50:33 -0300

    add: pantalla error y mi perfil

--- src/pages/Logged.jsx
+++ src/pages/Logged.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from "components/Header";
 
 export default function Logged() {
   return (
     <section className="vh-100">
-      <Header />
       <div className="flex-grow-1">
         <h1>Bienvenidos a la SEJU</h1>
       </div>
