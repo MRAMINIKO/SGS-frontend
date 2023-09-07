a43d618fdeb9e7e45d1d1435ac329506ce6efefd
FSI06 <ramimendoza13@gmail.com>
2023-09-07 18:05:56 -0300

    add new footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Header } from "@/components/Header/Header";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
+import { Footer } from "@/components/Footer/Footer";
 
 export const metadata = {
 	title: "Next.js",
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
 			<body>
 				<Header />
 				{children}
+				<Footer />
 			</body>
 		</html>
 	);
