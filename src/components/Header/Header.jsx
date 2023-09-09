b83ef647bb40a988cb3ea3338399e3d790013b10
Ramiro <67852209+MRAMINIKO@users.noreply.github.com>
2023-09-09 20:23:06 -0300

    refactor header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,20 @@
-import Image from 'next/image'
-import React from 'react'
-import logo from '@public/img/logo.svg';
-import menu from '@public/asset/menu.svg';
+import Image from "next/image";
+import React from "react";
+import logo from "@public/img/logo.svg";
+import { CustomOffcanvas } from "../Offcanvas/CustomOffcanvas";
+import Link from "next/link";
 
 export const Header = () => {
-    return (
-        <header className='container d-flex align-items-center justify-content-between sicky-top mb-5 pt-3'>
-            <div className='d-flex align-items-center'>
-                <Image src={logo} className='me-3' />
-                <h3 className='fw-bold'>SeJu</h3>
-            </div>
-            <Image src={menu} />
-        </header>
-    )
-}
+  return (
+    <header className="d-flex align-items-center justify-content-between sticky-top mb-5 px-2 py-3 text-bg-light">
+      <Link
+        href="/"
+        className="text-decoration-none text-dark d-flex align-items-center"
+      >
+        <Image src={logo} className="me-3" />
+        <h4 className="fw-bold">SeJu</h4>
+      </Link>
+      <CustomOffcanvas />
+    </header>
+  );
+};
