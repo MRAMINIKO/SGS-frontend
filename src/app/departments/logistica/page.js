e506700f96a60e11f235ceef03ca5e1b5161b8e4
Ramiro <67852209+MRAMINIKO@users.noreply.github.com>
2023-09-09 23:58:50 -0300

    refactor comienzo con pantalla de logistica

--- src/app/departments/logistica/page.js
+++ src/app/departments/logistica/page.js
@@ -1,5 +1,13 @@
 import React from "react";
+import logisticaIlustration from "@public/images/logistica-ilustration.png";
+import Image from "next/image";
+import styles from "./logistica.module.css";
 
 export default function LogisticaPage() {
-  return <div>Logística</div>;
+  return (
+    <section>
+      <h5 className={styles.title}>Logística</h5>
+      <Image src={logisticaIlustration} width={300} />
+    </section>
+  );
 }
